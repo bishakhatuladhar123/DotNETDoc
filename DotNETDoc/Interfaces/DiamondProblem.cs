e3517c42ff93e70a76d4e3a93043530f8435b36e
bishakhatuladhar123 <bishakhatuladhar123@gmail.com>
2026-04-26 15:49:41 +0545

    1 interface (diamond problem)

---
+++ DotNETDoc/Interfaces/DiamondProblem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Interfaces
+{
+    public class DiamondProblem
+    {
+        public static void Main()
+        {
+            ClassD classD = new ClassD(); //Note:•	There is conflict when we call the print method via ClassD. There will be confusion whether to use the Print method via ClassB or via ClassC
+            classD.Print();
+        }
+
+        public class ClassA
+        {
+            public virtual  void Print()
+            {
+                Console.WriteLine("ClassA");
+            }
+        }
+
+        public class ClassB: ClassA //(ClassB and ClassC) both inherit from ClassA. ClassB and ClassC override the Print method from ClassA differently
+        {
+            public override void Print()
+            {
+                Console.WriteLine("ClassB");
+            }
+        }
+
+        public class ClassC : ClassA 
+        {
+            public override void Print()
+            {
+                Console.WriteLine("ClassC");
+            }
+        }
+
+        public class ClassD: ClassB//,ClassC// Note: ClassD inherit from both (ClassB and ClassC) (but ClassD doesnot override the Print Method)
+        { 
+        
+        }
+
+
+    }
+}
+
+
+
---
+++ DotNETDoc/Interfaces/ResolveDiamondProblem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Interfaces
+{
+    public class ResolveDiamondProblem
+    {
+        public static void Main()
+        {
+            ClassD classD = new ClassD();//Note:Class D simply writes one version of Print method. There is no confusion because the interfaces didn't provide any conflicting logic—they just provided a requirement. Class D provides the actual code, so the compiler always knows exactly what to run
+            ((IClassA)classD).Print();
+            ((IClassB)classD).Print(); 
+            ((IClassC)classD).Print();
+            classD.Print();
+        }
+    }
+
+    interface IClassA
+    {
+        void Print();
+    }
+    interface IClassB:IClassA
+    {
+        new void Print();
+    }
+    interface IClassC:IClassA
+    {
+       new void Print();
+    }
+
+    public class ClassD : IClassB, IClassC
+    {
+        void IClassA.Print()
+        {
+            Console.WriteLine("Class A");
+
+        }
+        void IClassB.Print()//Note: this run only when called via IClassB reference
+        {
+            Console.WriteLine("Class  B");
+        }
+        void IClassC.Print()//Note: this run only when called via IClassC reference
+        {
+            Console.WriteLine("Class  C");
+
+        }
+        // Logic: ClassD decides exactly what happens.
+        // There is no more "confusion" because ClassD defines the behavior.
+        public void Print() =>Console.WriteLine("ClassD");
+    }
+}
