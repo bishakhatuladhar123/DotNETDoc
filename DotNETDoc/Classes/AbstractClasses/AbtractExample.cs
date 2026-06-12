371c51413d1878af4aa5071d545fe81c728e96e7
bishakhatuladhar123 <bishakhatuladhar123@gmail.com>
2026-06-12 15:52:41 +0545

    5 Class (Partial Class)

---
+++ DotNETDoc/Classes/AbstractClasses/AbtractExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Classes.AbstractClasses.AbstractExample
+{
+    public abstract class BaseProcessor
+    {
+        public void LogTransaction(double amount)
+        {
+            Console.WriteLine($"[Log Transaction] Rs.{amount}recorded at {DateTime.Now}");
+        }
+
+        public abstract void Pay(double amount);
+    }
+
+    public class KhaltiProcessor : BaseProcessor
+    {
+        public override void Pay(double amount)
+        {
+            LogTransaction(amount);
+            Console.WriteLine($"Paying Rs.{amount} via Khalti Processor");
+        }
+    }
+    public class EsewaProcessor : BaseProcessor
+    {
+        public override void Pay(double amount)
+        {
+            LogTransaction(amount);
+            Console.WriteLine($"Paying Rs.{amount} via Esewa Processor");
+        }
+    }
+
+    public class AbtractExample
+    {
+        public static void Main()
+        {
+            KhaltiProcessor khaltiProcessor = new KhaltiProcessor();
+            khaltiProcessor.Pay(500);
+
+            EsewaProcessor esewaProcessor = new EsewaProcessor();
+            esewaProcessor.Pay(800);
+        }
+    }
+}
---
+++ DotNETDoc/Classes/AbstractClasses/WithoutAbstractExample.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Classes.AbstractClasses
+{
+    public class KhaltiProcessor
+    {
+        public void LogTransaction(double amount)
+        {
+            Console.WriteLine($"[Log Transaction] Rs.{amount} recorded at {DateTime.Now}");
+        }
+        public void Pay(double amount)
+        {
+            Console.WriteLine($"Paying Rs.{amount} via Khalti REST api");
+        }
+    }
+    public class EsewaProcessor
+    {
+        public void LogTransaction(double amount)
+        {
+            Console.WriteLine($"[Log Transaction] Rs.{amount} recorded at {DateTime.Now}");
+        }
+        public void Pay(double amount)
+        {
+            Console.WriteLine($"Paying Rs.{amount} via Esewa REST api");
+        }
+    }
+
+    public class WithoutAbstractExample
+    {
+        public static void Main()
+        {
+            KhaltiProcessor khaltiProcessor = new KhaltiProcessor();
+            khaltiProcessor.LogTransaction(500);
+            khaltiProcessor.Pay(500);
+
+            EsewaProcessor esewaProcessor = new EsewaProcessor();
+            esewaProcessor.LogTransaction(800);
+            esewaProcessor.Pay(800);
+        }
+    }
+}
