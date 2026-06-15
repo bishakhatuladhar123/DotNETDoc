6d3108fc228b0253f68dea90ce9ad973ecde8ceb
bishakhatuladhar123 <bishakhatuladhar123@gmail.com>
2026-06-15 16:32:24 +0545

    5 Class (Partial Class)

---
+++ DotNETDoc/Classes/PartialClasses/StudentPCExample2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Classes.PartialClasses
+{
+    public partial class StudentPC
+    {
+        public void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+    }
+    
+}
---
+++ DotNETDoc/Classes/PartialClasses/TestPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Classes.PartialClasses
+{
+    public class TestPC
+    {
+        public static void Main()
+        {
+            StudentPC studentPC = new StudentPC();
+            studentPC.Method1();
+            studentPC.Method2();
+           // studentPC.Method3();
+        }
+    }
+
+    public class BookExample
+    { 
+        public void Method3()
+        {
+            Console.WriteLine("Method3");
+        }
+    }
+}
