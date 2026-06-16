533eb7312a75b019a034a240d241c904843fb81d
bishakhatuladhar123 <bishakhatuladhar123@gmail.com>
2026-06-16 16:08:19 +0545

    5 Class (Static Class)

---
+++ DotNETDoc/Classes/StaticClasses/StudentSCExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Classes.StaticClasses
+{
+    public static class StudentSCExample
+    {
+        //static field
+        public static string StudentName;
+
+        //static constructor
+         static StudentSCExample()
+        {
+
+        }
+        //static method
+        public static void Method1()
+        {
+
+        }
+    }
+
+    public class TeacherExample//:StudentSCExample
+    {
+
+    }
+}
---
+++ DotNETDoc/Classes/StaticClasses/TestSC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETDoc.Classes.StaticClasses
+{
+    public class TestSC
+    {
+        public static void Main()
+        {
+            //StudentSCExample studentSCExample = new StudentSCExample();
+            StudentSCExample.Method1();
+        }
+    }
+}
